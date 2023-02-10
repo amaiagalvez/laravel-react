533af07f9f314902c56cea7ecb8f8bc633da8858
amaiagalvez <amaiagalvez@hotmail.com>
2023-02-10 21:22:24 +0100

    heroicons install

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { BeakerIcon } from '@heroicons/react/24/solid'
 
 function App() {
 
   return (
-    <div className="bg-purple-400 text-3xl font-bold underline p-5">
-      App.jsx
+    <div className="bg-purple-400 text-3xl text-white font-bold underline p-5">
+      <BeakerIcon className="h-12 w-12 text-white" />
+      <span className="h-24 w-24">App.jsx</span>
     </div>
   )
 }
