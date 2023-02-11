6f402d740a2910b5abb7d93a580d287f8496645f
amaiagalvez <amaiagalvez@hotmail.com>
2023-02-11 23:12:23 +0100

    Surveys list

--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,11 +1,21 @@
 import PageComponent from "../components/PageComponent";
+import Survey from "../components/Survey";
+import { UseStateContext } from "../contexts/ContextProvider";
 
 export default function Surveys() {
+  const { surveys } = UseStateContext()
+
+  const onDeleteClick = () => {
+    console.log('on delete click')
+  }
+
   return (
-    <PageComponent
-      title="Surveys"
-    >
-      Children
+    <PageComponent title="Surveys">
+      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
+        {surveys.map(survey => (
+          <Survey survey={survey} key={survey.id} onDeleteClick={onDeleteClick} />
+        ))}
+      </div>
     </PageComponent>
   )
 }
